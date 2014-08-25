bf41862a62271e737522f9ad7ad239fab8cb7bd8
Sébastien Muller <igniweb.net@gmail.com>
2014-08-26 00:04:52 +0200

    First tests using 'it' and 'expect'

--- app/src/numbers.js
+++ app/src/numbers.js
@@ -4,7 +4,18 @@ define(function () {
 
     var self = {};
 
-    console.log('numbers');
+    self.add = function () {
+        var operands = Array.prototype.slice.call(arguments);
+        var total    = 0;
+
+        operands.forEach(function (value) {
+            total += parseInt(value);
+        });
+
+        return total;
+    };
+
+    console.log('Numbers module');
 
     return self;
 });
--- test/spec/numbers.js
+++ test/spec/numbers.js
@@ -1,10 +1,22 @@
-/* global define, describe */
+/* global define, describe, it, except */
 define(['numbers'], function (numbers) {
     'use strict';
 
     describe('The numbers module', function () {
         describe('The add method', function () {
+            it('should accept one or more numerical arguments and return the sum of them', function () {
+                // Arrange
+                var output;
+                var input1 = 1;
+                var input2 = 2;
 
+                // Act
+                output = numbers.add(input1, input2);
+
+                // Assert
+                expect(output).toEqual(3);
+                expect(output).not.toEqual(4);
+            });
         });
     });
 });
